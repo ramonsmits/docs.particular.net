5d79e7526019014cceb22a4b13a31815cfdf3011
Ramon Smits <ramon.smits@gmail.com>
2015-09-23 15:05:49 +0200

    Simplified OrderSubmittedHandler to make it compatible with NServiceBus v4

--- samples/sqltransport-nhpersistence/Version_2.1/Receiver/OrderSubmittedHandler.cs
+++ samples/sqltransport-nhpersistence/Version_2.1/Receiver/OrderSubmittedHandler.cs
@@ -4,31 +4,8 @@ using NHibernate;
 
 public class OrderSubmittedHandler : IHandleMessages<SubmitOrder>
 {
-    IBus bus;
-    ISession session;
-    
-    public OrderSubmittedHandler(IBus bus ,ISession session)
-    {
-        this.bus = bus;
-        this.session = session;
-    }
-
     public void Handle(SubmitOrder message)
     {
-        //Console.WriteLine("Order {0} worth {1} submitted", message.OrderId, message.Value);
-
         Runner.X.Signal();
-
-        //session.SaveOrUpdate(new Order
-        //{
-        //    OrderId = message.OrderId,
-        //    Value = message.Value
-        //});
-
-        //bus.Reply(new OrderAccepted
-        //{
-        //    OrderId = message.OrderId,
-        //});
-
     }
 }
