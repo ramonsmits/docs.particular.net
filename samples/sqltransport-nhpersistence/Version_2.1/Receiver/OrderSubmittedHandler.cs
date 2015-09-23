5dfca315ca6ecd588eb75e1f759f07bf212d75a4
Ramon Smits <ramon.smits@gmail.com>
2015-09-23 15:43:12 +0200

    Runner!!

--- samples/sqltransport-nhpersistence/Version_2.1/Receiver/OrderSubmittedHandler.cs
+++ samples/sqltransport-nhpersistence/Version_2.1/Receiver/OrderSubmittedHandler.cs
@@ -1,11 +1,10 @@
 using System;
 using NServiceBus;
-using NHibernate;
 
 public class OrderSubmittedHandler : IHandleMessages<SubmitOrder>
 {
     public void Handle(SubmitOrder message)
     {
-        Runner.X.Signal();
+        Runner.Signal();
     }
 }
