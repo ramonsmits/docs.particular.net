f57c4743416d804830d2edc4623d428f6d53b857
Ramon Smits <ramon.smits@gmail.com>
2017-03-01 11:07:52 +0100

    Added a simple handler that logs a received message.

---
+++ samples/show-case/on-premise/Core_6/Store.Shared/LogMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+class LogMessageHandler : IHandleMessages<object>
+{
+    static readonly bool IsDebugEnabled = Log.IsDebugEnabled;
+    static readonly ILog Log = LogManager.GetLogger("ReceiveStats");
+
+    public Task Handle(object message, IMessageHandlerContext context)
+    {
+        if (IsDebugEnabled)
+        {
+            var sentAt = DateTimeExtensions.ToUtcDateTime(context.MessageHeaders[Headers.TimeSent]);
+            var duration = DateTime.UtcNow - sentAt;
+            Log.DebugFormat("Received message '{0}' send at '{1}' took '{2}'", context.MessageId, sentAt, duration);
+        }
+        return Task.FromResult(0);
+    }
+}
