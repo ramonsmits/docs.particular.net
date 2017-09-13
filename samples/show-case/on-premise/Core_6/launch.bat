db118b572158d207337281979fd4c5b678d9158b
Ramon Smits <ramon.smits@gmail.com>
2017-09-13 15:03:16 +0200

    Batch file to easily launch endpoints.

---
+++ samples/show-case/on-premise/Core_6/launch.bat
@@ -0,0 +1,5 @@
+.\Store.ContentManagement\bin\Debug\Store.ContentManagement.exe
+.\Store.CustomerRelations\bin\Debug\Store.CustomerRelations.exe
+.\Store.Operations\bin\Debug\Store.Operations.exe
+.\Store.Sales\bin\Debug\Store.Sales.exe
+
