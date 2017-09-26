00106b90a183ef2030e0f3ed72c8d62ee3a99cd9
Ramon Smits <ramon.smits@gmail.com>
2017-09-26 23:06:26 +0200

    Now possible to run multiple instances where each instance will have its own sequential instance ID.

--- samples/show-case/on-premise/Core_6/Store.Shared/CommonConfiguration.cs
+++ samples/show-case/on-premise/Core_6/Store.Shared/CommonConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Text;
 using NServiceBus;
 using NServiceBus.Encryption.MessageProperty;
@@ -53,8 +55,36 @@ public static class CommonConfiguration
 
         transport.ApplyLabelToMessages(headers => (headers.ContainsKey(Headers.EnclosedMessageTypes) ? headers[Headers.EnclosedMessageTypes].Substring(0, Math.Min(200, headers[Headers.EnclosedMessageTypes].Length)) + "@" : string.Empty) + DateTime.UtcNow.ToString("O"));
 
+
+        var instanceId = Dns.GetHostName() + "-" + Lock.Instance;
+
 #pragma warning disable CS0618 // Type or member is obsolete
-        endpointConfiguration.EnableMetrics().SendMetricDataToServiceControl("Particular.ServiceControl.Monitoring", TimeSpan.FromSeconds(10));
+        endpointConfiguration.EnableMetrics().SendMetricDataToServiceControl("Particular.ServiceControl.Monitoring", TimeSpan.FromSeconds(10), instanceId: instanceId);
 #pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    static readonly FileInstanceLock Lock = new FileInstanceLock();
+}
+
+
+class FileInstanceLock
+{
+    FileStream f;
+    public int Instance { get; }
+
+    public FileInstanceLock()
+    {
+        do
+        {
+            try
+            {
+                var lockFileName = ".~" + ++Instance + ".lock";
+                f = File.Open(lockFileName, FileMode.Create, FileAccess.Write, FileShare.None);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        } while (f == null);
     }
 }
